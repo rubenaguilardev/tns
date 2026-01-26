558fb278bd74bde9c0f83bccfda780ad51b27180
Ruben Aguilar <ragui56@wgu.edu>
2026-01-26 04:06:03 -0800

    add main, style

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 const App = () => {
   return (
-    <h1 className="text-4xl">TNS</h1>
+    <main className="max-w-80 bg-[#F5F5F5] p-9 rounded-[10px]">
+      <div className="text-center">
+        <h1 className="font-bold text-[1.6rem] tracking-tight">Tenzies</h1>
+        <p className="text-[#4A4E74] font-['Inter']">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      </div>
+    </main>
   )
 }
 
