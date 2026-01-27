201701e684384450e15d6f129e7362da882e7825
Ruben Aguilar <ragui56@wgu.edu>
2026-01-26 17:31:59 -0800

    randomize die

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react"
+import Die from "./components/Die"
+
 const App = () => {
+
+  const [dice, setDice] = useState(generateAllNewDice())
+
+  function generateAllNewDice() {
+    return new Array(10)
+      .fill(0)
+      .map(() => Math.ceil(Math.random() * 6))
+  }
+  
+  const diceElements = dice.map(num => <Die value={num} />)
+
   return (
-    <main className="max-w-80 bg-[#F5F5F5] p-9 rounded-[10px]">
-      <div className="text-center">
+    <main className="max-w-80 flex flex-col bg-[#F5F5F5] p-9 rounded-[10px]">
+      <div className="text-center mb-7">
         <h1 className="font-bold text-[1.6rem] tracking-tight">Tenzies</h1>
         <p className="text-[#4A4E74] font-['Inter']">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      </div>
+      <div className="grid grid-cols-5 grid-rows-2 gap-4.5">
+        {diceElements}
       </div>
     </main>
   )
---
+++ src/components/Die.jsx
@@ -0,0 +1,9 @@
+const Die = ({value}) => {
+    return (
+        <button className="bg-white h-9 w-9 font-bold text-xl rounded-sm shadow-xl cursor-pointer">
+            {value}
+        </button>
+    )
+}
+
+export default Die
